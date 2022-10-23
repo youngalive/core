34b5ca3127fd3b0ca0fe06f77d2d957fcb7727f9
youngalive <02youngalive@gmail.com>
2022-10-24 07:09:59 +0900

    ApplicationContextInfoTest

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,7 +1,6 @@
 package hello.core;
 
 import hello.core.discount.DiscountPolicy;
-import hello.core.discount.FixDiscountPolicy;
 import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
@@ -12,10 +11,10 @@ import hello.core.order.OrderServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration // 설정정보
+// 설정정보
 public class AppConfig { // 객체의 생성과 연결을 담당 "관심사의 분리"
 
-    @Bean // 스프링 컨테이너에 등록됨
+    @Bean // 스프링 컨테이너에 등록됨, 빈 이름은 중복없이 생성
     public MemberService memberService() { // 구현 객체를 생성
         return new MemberServiceImpl(memberRepository()); // 생성자 주입 injection
     }
